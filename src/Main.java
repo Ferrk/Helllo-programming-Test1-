0a1d621c9cfba6b4890f5e27f3ffbd3ff968ca02
Ferrk <qeri4gyw@mail.ru>
2021-12-17 19:15:12 +0300

    add file

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main
 {
     public static void main(String[] args) {
-        System.out.println("Hello programming!");
+        int currentBalance = 2000000000;
+        int transferAmount = 500000000;
+        int totalSum = currentBalance + transferAmount;
+        System.out.println(totalSum);
     }
 }
